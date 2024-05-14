a9da86cb7d3e5575fb51b348dae4562edcac2de6
harshal508 <harshaldasarwar5088@gmail.com>
2024-05-14 22:22:16 +0530

    done

--- Server.js
+++ Server.js
@@ -3,10 +3,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// Placeholder function to simulate interaction with the Generative AI model
 async function generateQuestion(topic) {
   const prompt = `Write a question related to the topic: ${topic}.`;
 
@@ -21,11 +19,9 @@ async function generateQuestion(topic) {
     return text;
   } catch (error) {
     console.error("An error occurred while generating the question:", error);
-    return null; // Return null if an error occurs
+    return null; 
   }
 }
-
-// Placeholder function to simulate interaction with the Generative AI model for answer evaluation
 async function evaluateAnswer(question, answer) {
     const prompt = `Please evaluate the provided answer: "${answer}", in response to the question: "${question}". Provide a response indicating only the accuracy of the answer as a percentage.`;
 
@@ -40,7 +36,7 @@ async function evaluateAnswer(question, answer) {
       return text;
     } catch (error) {
       console.error("An error occurred while generating the question:", error);
-      return null; // Return null if an error occurs
+      return null; 
     }
   
 }
@@ -50,7 +46,7 @@ module.exports = {
   evaluateAnswer
 };
 
-// Express.js setup 
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -70,30 +66,24 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'user_interface.html'));
 });
 
-// Endpoint to handle request for generating a question
+
 app.post("/generate-question", async(req, res) => {
   const { topic } = req.body;
 
-  // Generate question based on the selected topic
   const generatedQuestion = await generateQuestion(topic);
   // console.log(generateQuestion)
 
-  // Send the generated question back to the frontend
   res.send({ question: generatedQuestion });
 });
 
-// Endpoint to handle request for evaluating an answer
 app.post("/evaluate-answer", async(req, res) => {
   const { question, answer } = req.body;
 
-  // Evaluate the provided answer
   const isCorrect = await evaluateAnswer(question, answer);
 
-  // Send the evaluation result back to the frontend
   res.send({ isCorrect });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
